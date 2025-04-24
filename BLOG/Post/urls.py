325b826a45d75b8302814bdb1ab21d6592a0f927
Nafyad <nafyadhailu99@gmail.com>
2025-04-24 23:22:12 +0300

    add post CRUD

--- BLOG/Post/urls.py
+++ BLOG/Post/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import  Post_create
+from .views import  create_post,blog_list,blog_detail
 
 urlpatterns = [
-    path('post_blog/',Post_create,name='Post_create' ),
+    path('post_blog/',create_post,name='create_post' ),
+    path('blog_list/',blog_list,name="blog_list"),
+    path('blog_detail <str:slug>/',blog_detail,name="blog_detail")
 ]
--- BLOG/Post/views.py
+++ BLOG/Post/views.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 from rest_framework import response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
-
+from Core.models import Post
+from django.shortcuts import get_object_or_404
 
 
 
@@ -20,11 +21,54 @@ from drf_spectacular.utils import extend_schema
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-def Post_create(request):
+def create_post(request):
+
     serializer= PostSerializer(data=request.data,context={'request':request})
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def blog_list(request):
+    post=Post.objects.filter(author=request.user)
+    serializer=PostSerializer(post,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+@extend_schema(
+        methods=['PUT'],
+    request=PostSerializer,
+    responses=PostSerializer
+)
+@api_view(['PUT','GET','DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def blog_detail(request,slug):
+    post = get_object_or_404(Post, author=request.user, slug=slug)
+
+    if request.method == 'GET':
+        serializer=PostSerializer(post)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer=PostSerializer(instance=post,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response({'message':'deleted successfully'})
+
+
+
+
+
+
+
 
 
